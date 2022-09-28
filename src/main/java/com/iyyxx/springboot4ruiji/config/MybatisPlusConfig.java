31d09b60466c25bf8481bef29d53d41119fabdc2
linzh <linzhihui@hotmail.com>
2022-09-28 17:00:11 +0800

    瑞吉项目 day02 任务完成

---
+++ src/main/java/com/iyyxx/springboot4ruiji/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.iyyxx.springboot4ruiji.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @className: MybatisPlusConfig
+ * @description: TODO 类描述
+ * @author: eric 4575252@gmail.com
+ * @date: 2022/9/28/0028 16:11:28
+ **/
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
